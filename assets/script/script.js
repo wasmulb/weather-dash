b7eb914d7c8d5cf6a6bdc3bc08af1a7426e8b3ca
Levi Wasmundt <leviwasmundt@gmail.com>
2022-12-05 11:55:14 -0800

    finished all functionality w/o LS

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,8 +2,9 @@ var submitButton = document.getElementById("submit-button")
 var cityInputEl = document.getElementById("text-input")
 var currentEl = document.getElementById("current-weather")
 var fiveDayEl = document.getElementById("five-day")
+var pastSearch = document.getElementById("past-search")
 //here
-//var testIcon = "10d"
+var testIcon = "10d"
 //var currentIcon = document.getElementById("current-icon").src="http://openweathermap.org/img/wn/"+testIcon+"@2x.png"
 var todaysDate = dayjs().format("MM/DD/YYYY")
 var cityURL = "http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}"
@@ -27,18 +28,21 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=da2b293b307
     })
     .then(function(data){
         currentEl.innerHTML=""
+        fiveDayEl.innerHTML=""
         console.log(data)
         var currentTempK = data.current.temp 
         var currentTempF = kToF(currentTempK)
         var currentHum = data.current.humidity
         var currentWind = data.current["wind_speed"]
-        var icon = data.current.weather[0].icon 
+        var icon = data.current.weather[0].icon;
+        var currentIcon = document.createElement("img");
+        currentIcon.setAttribute("src", "https://openweathermap.org/img/w/"+icon+".png");
         console.log(icon)
         var headerEl = document.createElement("h2")
         //var iconTestEl = document.innerHTML = "<img src = '//http://openweathermap.org/img/wn/10d@2x.png'>"
         headerEl.innerHTML = "Current Weather in " + city +" "+ todaysDate+":" 
         currentEl.appendChild(headerEl)
-        var currentIcon = document.getElementById("current-icon").src="http://openweathermap.org/img/wn/"+icon+"@2x.png"
+        //var currentIcon = document.getElementById("current-icon").setAttribute("src", "https://openweathermap.org/img/w/"+icon+".png");
         var tempEl = document.createElement("p")
         var windEl= document.createElement("p")
         var humEl = document.createElement("p")
@@ -55,6 +59,8 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=da2b293b307
             var forecastDiv = document.createElement("div")
             fiveDayEl.appendChild(forecastDiv)
             var forecastHeaderEl = document.createElement("h2")
+            var forecastIcon = document.createElement("img")
+            //forecastIcon.setAttribute("src", "https://openweathermap.org/img/w/"+icon+".png")
             var forecastTempEl = document.createElement("p")
             var forecastWindEl= document.createElement("p")
             var forecastHumEl = document.createElement("p")
@@ -65,9 +71,15 @@ fetch("http://api.openweathermap.org/geo/1.0/direct?q="+city+"&appid=da2b293b307
             var futureTempMinF = kToF(futureTempMinK)
             var forecastWind = data.daily[i]["wind_speed"]
             var forecastHum = data.daily[i].humidity
+            var futureIcon = data.daily[i].weather[0].icon
+            console.log(futureIcon)
+            forecastIcon.setAttribute("src", "https://openweathermap.org/img/w/"+futureIcon+".png")
+            forecastHeaderEl.innerHTML = dayjs().add(i+1, 'day').format("MM/DD/YYYY")
             forecastTempEl.innerHTML= "Temperature (Max/Min): "+futureTempMaxF+"F/"+futureTempMinF+"F"
             forecastWindEl.innerHTML= "Windspeed: "+forecastWind+" Kts"
             forecastHumEl.innerHTML = "Humidity: "+ forecastHum +"%"
+            forecastDiv.appendChild(forecastHeaderEl)
+            forecastDiv.appendChild(forecastIcon)
             forecastDiv.appendChild(forecastTempEl)
             forecastDiv.appendChild(forecastWindEl)
             forecastDiv.appendChild(forecastHumEl)
@@ -82,6 +94,17 @@ function kToF(kelvin){
     return far
 }
 
+function localStorageFun(){
+    var city = localStorage.getItem(city)
+    var pastEl = document.createElement("li")
+    pastEl.innerHTML = city
+    pastSearch.appendChild(pastEl)
+}
+
+localStorageFun()
+
 submitButton.addEventListener("click", function(){
     getCurrentWeather()
+    city = cityInputEl.value
+    localStorage.setItem(city, city)
 })
